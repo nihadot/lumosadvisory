37b0ea2be66cf547b97c60010c55842fd812a1e3
nihadot <mysonisfunny@gmail.com>
2024-09-26 13:35:50 +0530

    fixed buggs

--- src/Layouts/Layout/Layout.jsx
+++ src/Layouts/Layout/Layout.jsx
@@ -13,73 +13,14 @@ import Contact from "../Contact/Contact";
 import MobileMenu from "./MobileMenu"
 import { images } from "../../static";
 
+
 function Layout() {
-  const [browser, setBrowser] = useState('');
-  const [isFullScreen, setIsFullScreen] = useState(false);
-
-  useEffect(() => {
-    const detectBrowserAndFullScreen = () => {
-      const userAgent = navigator.userAgent.toLowerCase();
-      let detectedBrowser = '';
-
-      if (userAgent.indexOf('chrome') > -1 && userAgent.indexOf('edge') === -1 && userAgent.indexOf('edg') === -1 && userAgent.indexOf('opr') === -1) {
-        detectedBrowser = 'Chrome';
-      } else if (userAgent.indexOf('firefox') > -1) {
-        detectedBrowser = 'Firefox';
-      } else if (userAgent.indexOf('safari') > -1 && userAgent.indexOf('chrome') === -1) {
-        detectedBrowser = 'Safari';
-      } else if (userAgent.indexOf('edg') > -1) {
-        detectedBrowser = 'Edge';
-      } else if (userAgent.indexOf('opr') > -1 || userAgent.indexOf('opera') > -1) {
-        detectedBrowser = 'Opera';
-      } else {
-        detectedBrowser = 'Unknown Browser';
-      }
-
-      const fullScreenElement = document.fullscreenElement || 
-                                document.webkitFullscreenElement || 
-                                document.mozFullScreenElement || 
-                                document.msFullscreenElement;
-
-      const fullScreen = !!fullScreenElement;
-
-      // Debug information
-      console.log(`User Agent: ${navigator.userAgent}`);
-      console.log(`Browser: ${detectedBrowser}`);
-      console.log(`Document.fullscreenElement: ${document.fullscreenElement}`);
-      console.log(`document.webkitFullscreenElement: ${document.webkitFullscreenElement}`);
-      console.log(`document.mozFullScreenElement: ${document.mozFullScreenElement}`);
-      console.log(`document.msFullscreenElement: ${document.msFullscreenElement}`);
-      console.log(`Full Screen: ${fullScreen ? 'Yes' : 'No'}`);
-
-      setBrowser(detectedBrowser);
-      setIsFullScreen(fullScreen);
-    };
-
-    // Initial check
-    detectBrowserAndFullScreen();
-
-    // Add event listeners for full-screen changes
-    const handleFullScreenChange = () => detectBrowserAndFullScreen();
-
-    document.addEventListener('fullscreenchange', handleFullScreenChange);
-    document.addEventListener('webkitfullscreenchange', handleFullScreenChange); // Safari
-    document.addEventListener('mozfullscreenchange', handleFullScreenChange); // Firefox
-    document.addEventListener('MSFullscreenChange', handleFullScreenChange); // IE/Edge
-
-    // Cleanup listeners on component unmount
-    return () => {
-      document.removeEventListener('fullscreenchange', handleFullScreenChange);
-      document.removeEventListener('webkitfullscreenchange', handleFullScreenChange);
-      document.removeEventListener('mozfullscreenchange', handleFullScreenChange);
-      document.removeEventListener('MSFullscreenChange', handleFullScreenChange);
-    };
-  }, []);
+ 
   return (
     <>
       <div className=" w-full h-screen m-auto relative">
         <img src={images.layout} alt="" className=" block w-[68.778vh] h-screen object-cover m-0 p-0 absolute z-50 left-0 top-0" />
-        <img src={images.right} alt="" className="block  h-screen object-cover fixed z-[100] right-0 top-0 w-[9.222vh]" />
+        <img src={images.right} alt="" className="block  h-screen object-cover fixed z-[100] right-0 top-0 w-[10.222vh]" />
         <LeftBar/>
         {/* <MobileMenu/> */}
         <div className="main w-full min-h-screen ps-[18.556vh] pe-[8vh] absolute z-50">
